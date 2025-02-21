594d0c88a01a0859d4573c1098a24f90e77e7f5a
Thv 1364 <jmonson6@outlook.com>
2025-02-21 18:16:18 +0000

    d

---
+++ 1.py
@@ -0,0 +1,3 @@
+year = int(input("What year were you born? \n"))#What year were you extracted out of the womb?#What year did the average IQ of everyone on earth go down?
+year2 = int(input("What year do you want to know about? \n"))
+print("You will be " + str(year2 - year - 1) + " or " + str(year2 - year) + " years old in " + str(year2) + ", depending on the your birthday and the day given.")
